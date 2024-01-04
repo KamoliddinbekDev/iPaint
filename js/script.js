6654261e8227572c62d084ce7e25078aa4717492
Kamoliddinbek <kamoliddinxudoyberdiyevdev@gmail.com>
2024-01-04 16:25:49 +0500

    triangle draw

--- js/script.js
+++ js/script.js
@@ -1,29 +1,73 @@
-const canvas = document.querySelector("canvas");
-
+// Global variables
+const canvas = document.querySelector("canvas"),
+  toolBtns = document.querySelectorAll(".tool");
+// variables
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
-  brushWidth = 5;
+  brushWidth = 5,
+  selectedTool = "brush",
+  prevMouseX,
+  prevMouseY,
+  snapshot;
 
+//set canvas width and height
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
-const startDraw = () => {
+//start drawing
+const startDraw = (e) => {
   isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
+//stop drawing
 const stopDraw = () => {
   isDrawing = false;
 };
 
+//draw rectangle
+const drawRectangle = (e) => {
+  ctx.strokeRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
+
 const drawing = (e) => {
   if (!isDrawing) return;
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+  ctx.putImageData(snapshot, 0, 0);
+
+  switch (selectedTool) {
+    case "brush":
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      break;
+    case "rectangle":
+      drawRectangle(e);
+      break;
+    default:
+      break;
+  }
 };
+
+//set active class
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", stopDraw);
