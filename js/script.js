1a81198b3f665f2acc598ecf29277199c7e803b0
Kamoliddinbek <kamoliddinxudoyberdiyevdev@gmail.com>
2024-01-06 04:07:27 +0500

    set color from color picker

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 const canvas = document.querySelector("canvas"),
   toolBtns = document.querySelectorAll(".tool"),
   fillColor = document.querySelector("#fill-color"),
-  sizeSlider = document.querySelector("#size-slider");
+  sizeSlider = document.querySelector("#size-slider"),
+  colorBtns = document.querySelectorAll(".colors .option"),
+  colorPicker = document.querySelector("#color-picker");
 
 //variables with default value
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
   brushWidth = sizeSlider,
   selectedTool = "brush",
+  selectedColor = "#000",
   prevMouseX,
   prevMouseY,
   snapshot;
@@ -26,6 +29,8 @@ const startDraw = (e) => {
   prevMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
@@ -101,6 +106,24 @@ toolBtns.forEach((btn) => {
 
 //change brush size
 sizeSlider.addEventListener("change", () => (brushWidth = sizeSlider.value));
+
+//set color to shapes
+colorBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    const bgColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue("background-color");
+    selectedColor = bgColor;
+  });
+});
+
+//set color from color picker
+colorPicker.addEventListener("change", () => {
+  colorPicker.parentElement.style.background = colorPicker.value;
+  colorPicker.parentElement.click();
+});
 
 //stop drawing
 const stopDraw = () => {
